cb6786492956fb9c5d0722aeadfc143b1489b0a2
Pedro-Spindola <spindolapedrohenrique99@gmail.com>
2025-09-02 20:41:02 -0300

    Resolvendo os updates

--- Padrão de Projetos/Atividade_02_Adapter/back/src/main/java/com/spindola/atividade_02_pp/interfaces/IChipset.java
+++
@@ -1,9 +0,0 @@
-package com.spindola.atividade_02_pp.interfaces;
-
-import com.spindola.atividade_02_pp.enums.TipoConector;
-
-public interface IChipset {
-    public Integer getId();
-    public String processarDados();
-    public TipoConector getTipoConector();
-}
