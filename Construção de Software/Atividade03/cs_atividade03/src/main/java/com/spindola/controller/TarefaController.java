8b95c607219ffe31eb502237178e59d2a0e7e4d0
Pedro-Spindola <spindolapedrohenrique99@gmail.com>
2025-09-07 18:19:01 -0300

    Finalização Atividade_03_CC

--- Construção de Software/Atividade03/cs_atividade03/src/main/java/com/spindola/controller/TarefaController.java
+++ Construção de Software/Atividade03/cs_atividade03/src/main/java/com/spindola/controller/TarefaController.java
@@ -88,19 +88,82 @@ public class TarefaController {
     }
 
     private void atualizarTarefa(){
-        
+        System.out.println("\n--- Atualizar uma tarefa ---");
+        System.out.println("Informe o Id da tarefa que deseja atualizar!");
+        Integer id = lerInteiro("Digite ID: ");
+        String titulo = lerString("Titulo: ");
+        String descricao = lerString("Descrição: ");
+        for (Responsavel resp : service.listarResponsavel()){
+            System.out.println(resp.getId() + " - " + resp.getNome() + ".");
+        }
+        Integer responsavel = lerInteiro("Selecione responsavel: ");
+        System.out.println("Prioridade disponivel:");
+        System.out.println("1 - Baixa");
+        System.out.println("2 - Media");
+        System.out.println("3 - Alta");
+        Integer prioridade = lerInteiro("Selecione a prioridade: ");
+
+        Tarefa tarefaAtualizada = service.atualizarTarefa(id, titulo, descricao, responsavel, prioridade);
+        System.out.println("Tarefa atualizada com sucesso!");
+        System.out.println(tarefaAtualizada);
     }
 
     private void alterarStatusTarefa(){
-        
+        System.out.println("\n--- Atualizar status de uma tarefa ---");
+        System.out.println("Informe o Id da tarefa que deseja buscar!");
+        Integer idBusca = lerInteiro("Digite ID: ");
+        System.out.println("Status disponivel:");
+        System.out.println("1 - Pendente");
+        System.out.println("2 - Em Andamento");
+        System.out.println("3 - Concluído");
+        Integer status = lerInteiro("Selecione o status: ");
+        service.atualizarStatus(idBusca, status);
     }
 
     private void buscarPeloId(){
-        
+        System.out.println("\n--- Buscar pelo Id ---");
+        System.out.println("Informe o Id da tarefa que deseja buscar!");
+        Integer idBusca = lerInteiro("Digite ID: ");
+        Tarefa t = service.buscarPeloId(idBusca);
+        System.out.println("ID: " + t.getId());
+        System.out.println("Título: " + t.getTitulo());
+        System.out.println("Descrição: " + t.getDescricao());
+        System.out.println("Responsável: " + t.getResponsavel().getNome());
+        System.out.println("Status: " + t.getStatus());
+        System.out.println("Prioridade: " + t.getPrioridade());
+        System.out.println("Data de Criação: " + t.getDataCriacao().format(formatador));
+        System.out.println("Data de Conclusão: " + (t.getDataConclusao() != null ? t.getDataConclusao().format(formatador) : "Não concluída"));
+        System.out.println("-------------------------");
+
     }
 
     private void removerTarefa(){
-        
+        System.out.println("\n--- Remover tarefa ---");
+        List<Tarefa> tarefas = service.listarTarefas();
+        for (Tarefa t : tarefas) {
+            System.out.println("ID: " + t.getId());
+            System.out.println("Título: " + t.getTitulo());
+            System.out.println("Descrição: " + t.getDescricao());
+            System.out.println("Responsável: " + t.getResponsavel().getNome());
+            System.out.println("Status: " + t.getStatus());
+            System.out.println("Prioridade: " + t.getPrioridade());
+            System.out.println("Data de Criação: " + t.getDataCriacao().format(formatador));
+            System.out.println("Data de Conclusão: " + (t.getDataConclusao() != null ? t.getDataConclusao().format(formatador) : "Não concluída"));
+            System.out.println("-------------------------");
+        }
+
+        System.out.println("Informe o Id da tarefa que deseja remover!");
+        Integer idRemover = lerInteiro("Digite ID: ");
+        System.out.println("Desejar mesmo remover a tarefa selecionado com o ID: " + idRemover + ", digite S para sim, N para não.");
+        String confirmacaoString = lerString("Confirme: ");
+        Boolean confirmacao = confirmacaoString.equals("S") ? true : false;
+        if(confirmacao){
+            service.removerTarefa(idRemover);
+            System.out.println("Tarefa removida com sucesso.");
+        }else{
+            System.out.println("Operação cancelada.");
+        }
+
     }
 
     private void exibirMenu() {
--- Construção de Software/Atividade03/cs_atividade03/src/main/java/com/spindola/services/TarefaServices.java
+++ Construção de Software/Atividade03/cs_atividade03/src/main/java/com/spindola/services/TarefaServices.java
@@ -57,4 +57,68 @@ public class TarefaServices {
     public List<Responsavel> listarResponsavel(){
         return repositoryResponsavel.listarTodos();
     }
+
+    public Tarefa buscarPeloId(Integer id){
+        return repositoryTarefa.buscarPorId(id).get();
+    }
+
+    public boolean removerTarefa(Integer id){
+        if(repositoryTarefa.buscarPorId(id).isPresent()){
+            repositoryTarefa.removerTarefa(id);
+            return true;
+        }
+        return false;
+    }
+
+    public Tarefa atualizarTarefa(Integer id, String titulo, String descricao, Integer resp, Integer priordd){
+        if(!repositoryTarefa.buscarPorId(id).isPresent()){
+            throw new IllegalArgumentException("Tarefa não encontrada, ID inexistente.");
+        } 
+        if(!repositoryResponsavel.buscarPeloId(priordd).isPresent()){
+            throw new IllegalArgumentException("Responsavel escolhido inválido");
+        }
+        Responsavel responsavel = repositoryResponsavel.buscarPeloId(priordd).get();
+        PrioridadeTarefa prioridadeTarefa;
+
+        switch (priordd) {
+            case 1 -> prioridadeTarefa = PrioridadeTarefa.BAIXA;
+            case 2 -> prioridadeTarefa = PrioridadeTarefa.MEDIA;
+            case 3 -> prioridadeTarefa = PrioridadeTarefa.ALTA;
+            default -> throw new IllegalArgumentException("Opção de Prioridade inválido");
+        }
+
+        if(repositoryTarefa.buscarPorTitulo(titulo).size() > 1){
+            for(Tarefa t : repositoryTarefa.buscarPorTitulo(titulo)){
+                if(t.getResponsavel() == responsavel){
+                    throw new IllegalArgumentException("Já existe uma tarefa com o nome " + titulo + " designado para o responsavel " + responsavel.getNome());
+                }
+            }
+        }
+
+        Tarefa tarefaAtualizada = repositoryTarefa.buscarPorId(id).get();
+        tarefaAtualizada.setTitulo(titulo);
+        tarefaAtualizada.setDescricao(descricao);
+        tarefaAtualizada.setResponsavel(responsavel);
+        tarefaAtualizada.setPrioridade(prioridadeTarefa);
+        if(repositoryTarefa.atualizarTarefa(tarefaAtualizada)) return tarefaAtualizada;
+        return null;
+    }
+
+    public boolean atualizarStatus(Integer id, int intStatus){
+        if(!repositoryTarefa.buscarPorId(id).isPresent()){
+            throw new IllegalArgumentException("Tarefa não encontrada, ID inexistente.");
+        }
+        StatusTarefa status;
+        switch (intStatus) {
+            case 1 -> status = StatusTarefa.PENDENTE;
+            case 2 -> status = StatusTarefa.EM_ANDAMENTO;
+            case 3 -> status = StatusTarefa.CONCLUIDA;
+            default -> throw new IllegalArgumentException("Opção de Status inválido");
+        }
+        Tarefa tarefaAtualizada = repositoryTarefa.buscarPorId(id).get();
+        tarefaAtualizada.setStatus(status);
+        if(repositoryTarefa.atualizarTarefa(tarefaAtualizada)) return true;
+        return false;
+    }
+
 }
